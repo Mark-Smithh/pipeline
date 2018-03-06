036d0afde43fa48bdd56cf31baf4c16d223ff708
Smith, Mark <Mark.Smith391@T-Mobile.com>
2018-03-06 04:10:29 -0600

    remove echo

--- maven3-1-1/installMaven3_1_1.sh
+++ maven3-1-1/installMaven3_1_1.sh
@@ -1,16 +1,10 @@
 #wget http://mirrors.viethosting.vn/apache/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
 #tar -xf /apache-maven-3.1.1-bin.tar.gz  -C /usr/local
-echo 1
 cp -r apache-maven-3.1.1 /usr/local
-echo 2
 cd /usr/local
-echo 3
 rm -f maven
-echo 4
 ln -s apache-maven-3.1.1 maven
-echo 5
 rm -f /etc/profile.d/maven.sh
-echo 6
 echo "M2_HOME=/usr/local/maven" >> ~/.bashrc
 #echo "PATH=${M2_HOME}/bin:${PATH}" >> ~/.bashrc
 #echo "PATH=$PATH:${M2_HOME}/bin" >> ~/.bashrc
